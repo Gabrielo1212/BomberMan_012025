520d9b19d4e7d59e91cb1307f821fce2c38ef411
Gabrielo1212 <gabodrec2@gmail.com>
2025-03-10 03:23:12 -0400

    Forma de Power_Up y Uso de lifeSpan

--- Source/BomberMan_012025/Private/Power_Up.cpp
+++ Source/BomberMan_012025/Private/Power_Up.cpp
@@ -10,7 +10,14 @@ APower_Up::APower_Up()
 	PrimaryActorTick.bCanEverTick = true;
     // Crear el componente de mesh
     Mesh =CreateDefaultSubobject<UStaticMeshComponent>("BaseMeshComponent");
-    
+    // Establecer la mesh
+    auto MeshAsset = ConstructorHelpers::FObjectFinder<UStaticMesh>(TEXT("StaticMesh'/Engine/EngineMeshes/Sphere.Sphere\'"));
+    if (MeshAsset.Object != nullptr)
+    {
+    Mesh->SetStaticMesh(MeshAsset.Object);
+    // Reducir el tamaño de la malla de esfera
+    Mesh->SetRelativeScale3D(FVector(0.5f, 0.5f, 0.5f));
+    }
 
 }
 
